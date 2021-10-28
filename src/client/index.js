61df69d937dadd4798e0f1db96937c05b9ab5382
ahmed7fathi <ahmed777fathi@gmail.com>
2021-10-28 23:55:36 +0200

    adding sass to webpack adding style to index.html with sass

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,4 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import './styles/main.scss';
 
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
